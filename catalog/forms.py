045919a7f9f6d9c0b2aa69590be170852ca6b765
Anastasia <antipinaanastasia0112@gmail.com>
2025-08-09 08:12:57 +0300

    add CRUD for Product

---
+++ catalog/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Product
+
+black_list_words = {
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+}
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields["name"].widget.attrs.update(
+            {"placeholder": "Введите наименование продукта"}
+        )
+        self.fields["description"].widget.attrs.update(
+            {"placeholder": "Введите описание продукта"}
+        )
+        self.fields["price"].widget.attrs.update(
+            {"placeholder": "Введите стоимость продукта"}
+        )
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price < 0:
+            raise ValidationError("Цена продукта не может быть отрицательной")
+        return price
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+        for word in black_list_words:
+            if name and word in name.lower():
+                self.add_error("name", "В названии продукта есть ошибка")
+            if description and word in description.lower():
+                self.add_error("description", "В описании продукта есть ошибка")
+
+    class Meta:
+        model = Product
+        fields = "__all__"
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 
-from .views import ContactsTemplateView, ProductDetailView, ProductListView
+from .views import (ContactsTemplateView, ProductCreateView, ProductDeleteView,
+                    ProductDetailView, ProductListView, ProductUpdateView)
 
 app_name = CatalogConfig.name
 
@@ -10,4 +11,11 @@ urlpatterns = [
     path("home/", ProductListView.as_view(), name="home"),
     path("product_page/<int:pk>/", ProductDetailView.as_view(), name="product_page"),
     path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
+    path("product/new/", ProductCreateView.as_view(), name="product_form"),
+    path(
+        "product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"
+    ),
+    path(
+        "product/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"
+    ),
 ]
